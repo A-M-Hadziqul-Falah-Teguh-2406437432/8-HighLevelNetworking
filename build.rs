67c29442e93c398e49334f0fe037bdb5d17365c1
FalahHadziqul <hadziqulfalah18@gmail.com>
2026-05-05 09:17:39 +0700

    Payment Service Implementation

--- build.rs
+++ build.rs
@@ -1,7 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
+    let protoc = protoc_bin_vendored::protoc_bin_path()?;
+    std::env::set_var("PROTOC", protoc);
+
+    tonic_prost_build::configure()
         .build_server(true)
-        .compile(
+        .compile_protos(
             &["proto/services.proto"], // Path to your proto file
             &["proto"], // Directory where the proto file is located
         )?;
